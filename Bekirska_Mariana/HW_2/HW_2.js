3736569a2c9eda89ff38707ae1b41f8f483345ba
mariana-bekirska-moc <157789486+marianabekirska@users.noreply.github.com>
2025-06-30 22:44:23 +0300

    Coding Task #1: Numbers

---
+++ Bekirska_Mariana/HW_2/HW_2.js
@@ -0,0 +1,10 @@
+const num = 12;
+if (num >10 && num % 2 === 0 ) { 
+ console.log("Число парне і більше за 10");
+} else {
+    console.log("Число непарне і менше за 10")
+}
+
+for (let i=1; i<=num; i=i+1) {
+    console.log(i);
+}
